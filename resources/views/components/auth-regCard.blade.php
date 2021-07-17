6bf85c78807947fa96aef162d6f96985e6febda4
DanSanz09 <84282856+dan2xsanz@users.noreply.github.com>
2021-07-17 12:04:47 +0800

    Create auth-regCard.blade.php

---
+++ resources/views/components/auth-regCard.blade.php
@@ -0,0 +1,27 @@
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+<p class="fw-bold">Register:</p> 
+    <STYle>
+        .fw-bold
+        {
+            opacity: 0.5;
+            font-size:  80px;
+            font-family: "Times New Roman", Times, serif;
+            color: #007aff;
+            text-shadow: 3px 3px 3px black;
+            
+        }
+        .fw-bold 
+        {
+          
+            width: 23%;
+           height: 69px;
+                }
+    </STYle>
+
+    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+        {{ $slot }}
+       
+    </div>
+   
+
+</div>
