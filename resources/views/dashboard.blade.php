bead5c9be28330f25683e10ca2882d505248a1f0
DanSanz09 <84282856+dan2xsanz@users.noreply.github.com>
2021-07-17 12:13:22 +0800

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h1 class="font-semibold text-xl text-gray-800 leading-tight">
-        Hellow Admin 
+            Hellow Admin 
         </h1>
     </x-slot>
 
